3c1656d5544f6f19fe5c65d82665dd5bc9690585
AndreyDuda <andreyduda88@gmail.com>
2019-03-31 23:06:06 +0300

    add customizer site

--- wp-content/themes/coffeelove/inc/functions/customizer_functions.php
+++ wp-content/themes/coffeelove/inc/functions/customizer_functions.php
@@ -1,5 +1,21 @@
 <?php
 
-add_action('customize_register', function () {
+add_action('customize_register', function($customizer) {
+    $customizer->add_section(
+        'section_one', array(
+            'title' => 'Настройки сайта',
+            'description' => '',
+            'priority' => 11,
+        )
+    );
+    $customizer->add_setting('phone',
+        array('default' => '89215555555')
+    );
 
+    $customizer->add_control('phone', array(
+            'label' => 'Телефон',
+            'section' => 'section_one',
+            'type' => 'text',
+        )
+    );
 });
