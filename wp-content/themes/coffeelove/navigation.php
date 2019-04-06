6fda4be1563330ad2bd932bda2b1e08c8f44d397
AndreyDuda <andreyduda88@gmail.com>
2019-04-06 13:10:01 +0300

    single + navigation

---
+++ wp-content/themes/coffeelove/navigation.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	<div class="container">
+
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a href="" class="navbar-logo">
+				<img src="<?php echo get_template_directory_uri() ?>/assets/image/logo_catalog.png.pagespeed.ce.C22KUBTLun.png" alt="Coffee-Love для любителей кофе">
+			</a>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#intro">Главная</a></li>
+				<li><a href="#how-it-works">Как мы работаем</a></li>
+				<li><a href="#stats">Статистика</a></li>
+				<li><a href="#about_us">О нас</a></li>
+				<li><a href="#coffee">Кофе</a></li>
+				<li><a href="#coffee-machines">АРЕНДА КОФЕМАШИН</a></li>
+				<li><a href="#clients">Клиенты</a></li>
+				<li><a href="#footer">Контакты</a></li>
+			</ul>
+		</div>
+	</div>
+
+</nav>
