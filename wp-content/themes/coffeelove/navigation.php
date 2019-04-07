b829314427e9e5c2e85c06781caf8ab739ff0faf
AndreyDuda <andreyduda88@gmail.com>
2019-04-08 00:28:02 +0300

    correct style

--- wp-content/themes/coffeelove/navigation.php
+++ wp-content/themes/coffeelove/navigation.php
@@ -9,7 +9,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a href="" class="navbar-logo">
+			<a href="" class="navbar-logo" style="display: none;">
 				<img src="<?php echo get_template_directory_uri() ?>/assets/image/logo_catalog.png.pagespeed.ce.C22KUBTLun.png" alt="Coffee-Love для любителей кофе">
 			</a>
 		</div>
@@ -18,14 +18,14 @@
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav navbar-right">
                 <?php $link = (!is_home())? home_url() . '/':''; ?>
-				<li><a href="<?php echo $link;?>#intro">Главная </a></li>
-				<li><a href="<?php echo $link;?>#how-it-works">Как мы работаем</a></li>
-				<li><a href="<?php echo $link;?>#stats">Статистика</a></li>
-				<li><a href="<?php echo $link;?>#about_us">О нас</a></li>
-				<li><a href="<?php echo $link;?>#coffee">Кофе</a></li>
-				<li><a href="<?php echo $link;?>#coffee-machines">АРЕНДА КОФЕМАШИН</a></li>
-				<li><a href="<?php echo $link;?>#clients">Клиенты</a></li>
-				<li><a href="<?php echo $link;?>#footer">Контакты</a></li>
+				<li><a style="color: white;" href="<?php echo $link;?>#intro">Главная </a></li>
+				<li><a style="color: white;" href="<?php echo $link;?>#how-it-works">Как мы работаем</a></li>
+				<li><a style="color: white;" href="<?php echo $link;?>#stats">Статистика</a></li>
+				<li><a style="color: white;" href="<?php echo $link;?>#about_us">О нас</a></li>
+				<li><a style="color: white;" href="<?php echo $link;?>#coffee">Кофе</a></li>
+				<li><a style="color: white;" href="<?php echo $link;?>#coffee-machines">АРЕНДА КОФЕМАШИН</a></li>
+				<li><a style="color: white;" href="<?php echo $link;?>#clients">Клиенты</a></li>
+				<li><a style="color: white;" href="<?php echo $link;?>#footer">Контакты</a></li>
 			</ul>
 		</div>
 	</div>
